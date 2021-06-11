9c4a9903068d2add48721596c249d7dc7fcd4903
6urrp <sndrtasic@gmail.com>
2021-06-11 19:18:26 +0200

    adding ground moving to super mario

--- 05_javascript/DOM_events/task4/index.js
+++ 05_javascript/DOM_events/task4/index.js
@@ -2,10 +2,9 @@ var bodyContainer = document.querySelector("body")
 var mario = document.querySelector(".mario");
 var container = document.querySelector(".main-container");
 var xPosition = 0;
-var yPosition = "0px";
+var intervalId;
 
 var moveRight = function (e) {
-        console.log(e);
         if (e.keyCode === 39) {
                 mario.setAttribute("src", "./img/mario_running.gif");
         }  
@@ -19,5 +18,24 @@ var stopMoving = function (e) {
         bodyContainer.addEventListener("keydown", moveRight);
 }
 
+var groundMoving = function (e) {
+        if (e.keyCode === 39) {
+                intervalId = setInterval(function() {
+                        xPosition -= 1;
+                        container.style.backgroundPosition = xPosition + 'px bottom';  
+                },1);
+                bodyContainer.removeEventListener("keydown", groundMoving);
+        }
+}
+
+var groundStop = function (e) {
+        if (e.keyCode === 39) {
+                clearInterval(intervalId);
+        }
+        bodyContainer.addEventListener("keydown", groundMoving);
+}
+
 bodyContainer.addEventListener("keydown", moveRight);
+bodyContainer.addEventListener("keydown", groundMoving);
 bodyContainer.addEventListener("keyup", stopMoving);
+bodyContainer.addEventListener("keyup", groundStop);
