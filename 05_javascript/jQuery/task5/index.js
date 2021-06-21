a8f1e3d9e08802ed611174e208b9fb17bce3363e
6urrp <sndrtasic@gmail.com>
2021-06-21 20:56:56 +0200

    Changing event to mouseover click

--- 05_javascript/jQuery/task5/index.js
+++ 05_javascript/jQuery/task5/index.js
@@ -2,14 +2,14 @@ var $border = $("img.selected");
 var $img = $border.parent().next().children()[1];
 
 
-$($img).on("click", function () {
+$($img).on("mouseover click", function () {
         $border.css({
             "border" : "none"
         })
         $($img).addClass("selected") 
 })
 
-$($border).on("click", function() {
+$($border).on("mouseover click", function() {
     $($img).removeClass("selected");
     $border.css("border", "10px solid red")
 })
