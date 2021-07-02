14f1dcfe9f400c58050bce7723daf8d4cc56bf45
6urrp <sndrtasic@gmail.com>
2021-07-02 20:01:56 +0200

    JSON exercise with dog API finished

---
+++ 05_javascript/05_ajax&JSON/task2/main.js
@@ -0,0 +1,24 @@
+var $button = document.querySelector("#submit");
+var $img = document.querySelector(".img-paragraph")
+var $dog = document.querySelector(".dog")
+var quotes = ["Pet me, human!", "Did you say walk?!", "Feed me, human!", "Ciao, ciao!", "Wuff!", "Did you say bacon!?", "I'm a party animal!"]
+
+
+var handler = function () {
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://dog.ceo/api/breeds/image/random", true);
+
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            var data = JSON.parse(request.response);
+            $img.setAttribute("src", data.message);
+        }
+    };
+
+    request.send();
+    $dog.textContent = quotes[Math.floor(Math.random()*quotes.length)]
+
+}
+
+$button.addEventListener("click", handler)
+
