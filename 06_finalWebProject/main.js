67622a2a65de1d920dfd88fa3bdcb4296fc88357
6urrp <sndrtasic@gmail.com>
2021-07-07 21:19:01 +0200

    Changing from jquery to vanilla js/ improving logic/ adding sass

--- 06_finalWebProject/main.js
+++ 06_finalWebProject/main.js
@@ -1,26 +1,39 @@
 const endpoint = "http://api.tvmaze.com/shows";
-let $main = $("main");
-let $top50 ={};
+let main = document.querySelector(".main-div");
+let top50 ={};
 
 const xhr = new XMLHttpRequest();
 xhr.open("GET", endpoint, true);
 xhr.onload = function () {
     const request = JSON.parse(xhr.responseText);
-    var $rating = request.sort(function(a, b) {
+    let rating = request.sort(function(a, b) {
         return b.rating.average - a.rating.average;
     })
-    $top50 = $rating.slice(0, 50);
-    $top50.forEach(function(el){
-        $main.append(
-            `
-            <div>
-            <h5>${el.name}</h5>
-            <img src="${el.image.medium}" class="">
-            </div>
-            `
-        )
+    top50 = rating.slice(0, 50);
+    top50.forEach(function(el){
+        let mainDiv = document.createElement("div");
+        mainDiv.setAttribute("data-key", el.id);
+        mainDiv.classList = "tv-div";
+        let img = document.createElement("img");
+        img.setAttribute("src", el.image.medium);
+        let title = document.createElement("h4");
+        title.textContent = el.name;
+        main.append(mainDiv);
+        mainDiv.append(img)
+        mainDiv.append(title)
     })
-    console.log(top50)
+
+    localStorage.clear();
+
+    var div = document.querySelectorAll(".tv-div");
+    div.forEach(function(el) {
+        el.addEventListener("click", function(event) {
+            event.preventDefault();
+            let key = document.querySelector(".tv-div").getAttribute("data-key");
+            window.localStorage.setItem("id", key);
+            document.location = "tv-show.html"
+        })
+    })
 
 }
 xhr.send()
---
+++ 06_finalWebProject/second.js
@@ -0,0 +1,13 @@
+// console.log(localStorage)
+// var a = localStorage.getItem("id");
+// console.log(a)
+
+// var request = new XMLHttpRequest();
+
+// request.open("GET", "http://api.tvmaze.com/shows/" + a + "/seasons")
+
+// request.onload = function() {
+//     var response = JSON.parse(request.responseText);
+//     console.log(response)
+// }
+// request.send();
